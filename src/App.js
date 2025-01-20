367d1d64b82f1cbab98b0cadac62d8b546f73e86
pratham3115 <jpratham134@gmail.com>
2025-01-20 18:09:08 +0530

    chnages commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './customer/components/navigation/navigation';
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div>
+      <Navbar />
+      {/* Other components */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
